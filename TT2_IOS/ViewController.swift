995bc63ad70e286dfccd6f5f9f83cec2b44c645a
“KaiHao” <okh8609@gmail.com>
2019-05-20 03:02:35 -0700

    Initial Commit

---
+++ TT2_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TT2_IOS
+//
+//  Created by KaiHao  on 2019/5/20.
+//  Copyright © 2019 NTUST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
