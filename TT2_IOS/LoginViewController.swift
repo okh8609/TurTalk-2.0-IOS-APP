091b385c0928e4e424f6edc3b8b07f9cff8f9ec3
KaiHao <okh8609@gmail.com>
2019-05-21 05:12:33 -0700

    reg ui, invite ui

---
+++ TT2_IOS/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  TT2_IOS
+//
+//  Created by KaiHao  on 2019/5/21.
+//  Copyright © 2019 NTUST. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
